591bb76a24c7dc0d78c4f2efe641c06e6dbc66db
Jonathan Lehr <jonathan@aboutobjects.com>
2020-05-04 13:12:57 -0400

    Added ANSI C Labs

---
+++ ANSI C Labs/ANSI C Labs/main.c
@@ -0,0 +1,18 @@
+#include "ANSICBasics.h"
+#include "Pointers.h"
+
+
+int main(int argc, const char *argv[])
+{
+    RunANSICBasics01();
+    RunANSICBasics02();
+    
+    RunPointers01();
+    RunPointers02();
+    RunPointers03();
+    RunPointers04();
+    RunPointers05();
+
+    return 0;
+}
+

f6e800683c3f2df68695d35a04d49259f02ecbd1
Jonathan Lehr <jonathan@aboutobjects.com>
2020-05-06 20:55:00 -0400

    Wednesday evening WIP

--- Coolness/Coolness/CLNCoolViewCell.m
+++ Coolness/Coolness/CLNCoolViewCell.m
@@ -48,14 +48,23 @@ const UIEdgeInsets CLNTextInsets = {
 
 // MARK: - Animation
 
-+ (void)animateWithDuration:(NSTimeInterval)duration
-                 animations:(void (^)(void))animations {
-    
-}
-
-
 - (void)bounce {
     NSLog(@"In %s", __func__);
+    [self animateBounceWithDuration:1 size:CGSizeMake(120, 240)];
+}
+
+- (void)configureBounceAnimationWithSize:(CGSize)size {
+    [UIView setAnimationRepeatAutoreverses:YES];
+    [UIView setAnimationRepeatCount:3];
+    CGAffineTransform translation = CGAffineTransformMakeTranslation(size.width, size.height);
+    self.transform = CGAffineTransformRotate(translation, M_PI_2);
+}
+
+- (void)animateBounceWithDuration:(NSTimeInterval)duration size:(CGSize)size {
+    typeof(self) __weak weakSelf = self;
+    [UIView animateWithDuration:duration
+                     animations:^{ [weakSelf configureBounceAnimationWithSize:size]; }
+                     completion:^(BOOL finished) { weakSelf.transform = CGAffineTransformIdentity; }];
 }
 
 // MARK: - Drawing and resizing
--- Coolness/Coolness/CLNCoolViewController.m
+++ Coolness/Coolness/CLNCoolViewController.m
@@ -4,24 +4,78 @@
 #import "CLNCoolViewController.h"
 #import "CLNCoolViewCell.h"
 
+@interface CLNCoolViewController () <UITextFieldDelegate>
+@property (strong, nonatomic) UITextField *textField;
+@property (strong, nonatomic) UIView *contentView;
+@end
+
 @implementation CLNCoolViewController
+
+- (void)addCell {
+    NSLog(@"In %s", __func__);
+    CLNCoolViewCell *newCell = [[CLNCoolViewCell alloc] init];
+    newCell.text = self.textField.text;
+    newCell.backgroundColor = UIColor.blueColor;
+    [self.contentView addSubview:newCell];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSLog(@"In %s", __func__);
+    [textField resignFirstResponder];
+    return YES;
+}
 
 - (void)loadView {
     UIView *backgroundView = [[UIView alloc] init];
     self.view = backgroundView;
     
-    CLNCoolViewCell *subview1 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(20, 100, 200, 40)];
-    CLNCoolViewCell *subview2 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(50, 160, 200, 40)];
+    CGRect screenRect = UIScreen.mainScreen.bounds;
+    CGRect accesssoryRect;
+    CGRect contentRect;
+    CGRectDivide(screenRect, &accesssoryRect, &contentRect, 100, CGRectMinYEdge);
     
-    subview1.text = @"Hello World! 🌎🌏🌍";
-    subview2.text = @"Cool Cells FTW!!! 🥂🍾";
+    UIView *accessoryView = [[UIView alloc] initWithFrame:accesssoryRect];
+    self.contentView = [[UIView alloc] initWithFrame:contentRect];
     
-    [backgroundView addSubview:subview1];
-    [backgroundView addSubview:subview2];
+    self.contentView.clipsToBounds = YES;
+    
+    [backgroundView addSubview:accessoryView];
+    [backgroundView addSubview:self.contentView];
+    
+    // Controls
+    
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(15, 50, 240, 30)];
+    self.textField.placeholder = @"Enter some text";
+    self.textField.borderStyle = UITextBorderStyleRoundedRect;
+    
+    self.textField.delegate = self;
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"Add Cell" forState:UIControlStateNormal];
+    [button sizeToFit];
+    button.frame = CGRectOffset(button.frame, 265, 50);
+    
+    [button addTarget:self action:@selector(addCell) forControlEvents:UIControlEventTouchUpInside];
+    
+    [accessoryView addSubview:self.textField];
+    [accessoryView addSubview:button];
+    
+    // Cool Cells
+    CLNCoolViewCell *cell1 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(20, 100, 200, 40)];
+    CLNCoolViewCell *cell2 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(50, 160, 200, 40)];
+    
+    cell1.text = @"Hello World! 🌎🌏🌍";
+    cell2.text = @"Cool Cells FTW!!! 🥂🍾";
+    
+    [self.contentView addSubview:cell1];
+    [self.contentView addSubview:cell2];
+    
+    accessoryView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.6];
+    self.contentView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.4];
     
     backgroundView.backgroundColor = UIColor.brownColor;
-    subview1.backgroundColor = UIColor.purpleColor;
-    subview2.backgroundColor = UIColor.orangeColor;
+    cell1.backgroundColor = UIColor.purpleColor;
+    cell2.backgroundColor = UIColor.orangeColor;
 }
 
 @end
